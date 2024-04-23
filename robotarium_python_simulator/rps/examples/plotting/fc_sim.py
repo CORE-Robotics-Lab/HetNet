5fc4cba7be60b08acd17acf8f3fc9ab81bc39789
Rohan Paleja <rohanpaleja123@gmail.com>
2024-04-22 23:54:36 -0400

    robot-demo readme

---
+++ robotarium_python_simulator/rps/examples/plotting/fc_sim.py
@@ -0,0 +1,99 @@
+import rps.robotarium as robotarium
+from rps.utilities.transformations import *
+from rps.utilities.barrier_certificates import *
+from rps.utilities.misc import *
+from rps.utilities.controllers import *
+
+import numpy as np
+import time
+
+pred1_locations = [[2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 4], [2, 4], [1, 4], [1, 4], [0, 4], [0, 4], [1, 4], [0, 4], [0, 4], [0, 4], [1, 4], [1.03, 4], [2, 4], [3, 4], [3, 4], [3, 4], [1, 3]]
+pred2_locations = [[0, 2], [0, 3], [0, 3], [1, 3], [1, 4], [1, 4], [1, 4], [0, 4], [0, 4], [0, 4], [0, 4], [1, 4], [0, 4], [0, 4], [0, 4], [1, 4], [.98, 4], [2, 4], [3, 4], [3, 4], [3, 4], [3, 1]]
+action1_locations = [[1, 1], [2, 1], [2, 1], [2, 2], [1, 2], [1, 3], [1, 4], [1, 4], [0, 4], [0, 4], [0, 4], [0, 3], [1, 3], [0, 3], [0, 3], [1, 3], [1, 4], [2, 4], [2, 4], [3, 4], [3, 4], [4, 2]]
+fire1_locations = [[3, 4], [2, 4], [1, 4], [0, 4], [0, 4], [0, 4], [0, 4], [0, 4], [0, 3], [0, 3], [0, 3], [0, 3], [0, 3], [0, 3], [2, 4], [2, 4], [2, 4], [2, 4], [3, 4], [3, 4], [0, 4]]
+fire2_locations = [None, [3, 4], [2, 4], [1, 4], [1, 4], [1, 4], [1, 4], [2, 4], [0, 4], [0, 4], [2, 4], [2, 4], [2, 4], [2, 4], [3, 4], [3, 4], [3, 4], [3, 4], None, None, None]
+fire3_locations = [None, None, [3, 4], [2, 4], [2, 4], [2, 4], [2, 4], [3, 4], [2, 4], [2, 4], [3, 4], [3, 4], [3, 4], [3, 4], None, None, None, None, None, None, None]
+fire4_locations = [None, None, None, [3, 4], [3, 4], [3, 4], [3, 4], None, [3, 4], [3, 4], None, None, None, None, None, None, None, None, None, None, None]
+
+for e, (coord0, coord1) in enumerate(pred1_locations):
+    pred1_locations[e][0] = coord0 - 2.5
+    pred1_locations[e][1] = coord1 - 2.5
+
+for e, (coord0, coord1) in enumerate(pred2_locations):
+    pred2_locations[e][0] = coord0 - 2.5
+    pred2_locations[e][1] = coord1 - 2.5
+
+for e, (coord0, coord1) in enumerate(action1_locations):
+    action1_locations[e][0] = coord0 - 2.5
+    action1_locations[e][1] = coord1 - 2.5
+def create_goal_points(i):
+    goal_points = np.zeros((3,3))
+    goal_points[:2,:] = scaling_factor * np.array([pred1_locations[i], pred2_locations[i], action1_locations[i]]).T
+    return goal_points
+
+# Instantiate Robotarium object
+N = 3
+scaling_factor = .5
+# initial_conditions = np.zeros((3,3))
+# initial_conditions[:2,:] = scaling_factor * np.array([pred1_locations[0], pred2_locations[0], action1_locations[0]]).T #np.array(np.mat('1 0.5 -0.5 0; 0.8 -0.3 -0.75 0.1; 0 0 0 0'))
+#
+initial_conditions = create_goal_points(0)
+r = robotarium.Robotarium(number_of_robots=N, show_figure=True, initial_conditions=initial_conditions, sim_in_real_time=False)
+
+# Define goal points by removing orientation from poses
+goal_points = np.zeros((3,3))
+goal_points[:2,:] = scaling_factor * np.array([pred1_locations[1], pred2_locations[1], action1_locations[1]]).T
+# goal_points = 0.9*np.array([[-1, 1, -1],[-1, -1, 1],[0,0,0]])
+
+# Create single integrator position controller
+single_integrator_position_controller = create_si_position_controller()
+
+# Create barrier certificates to avoid collision
+#si_barrier_cert = create_single_integrator_barrier_certificate()
+si_barrier_cert = create_single_integrator_barrier_certificate_with_boundary()
+
+_, uni_to_si_states = create_si_to_uni_mapping()
+
+# Create mapping from single integrator velocity commands to unicycle velocity commands
+si_to_uni_dyn = create_si_to_uni_dynamics_with_backwards_motion()
+
+#Read in and scale image
+gt_img = plt.imread('GTLogo.png')
+x_img = np.linspace(-1.0, 1.0, gt_img.shape[1])
+y_img = np.linspace(-1.0, 1.0, gt_img.shape[0])
+
+gt_img_handle = r.axes.imshow(gt_img, extent=(-1, 1, -1, 1))
+
+# define x initially
+x = r.get_poses()
+x_si = uni_to_si_states(x)
+r.step()
+
+# While the number of robots at the required poses is less
+# than N...
+for i in range(1, 21):
+    print(i)
+    goal_points = create_goal_points(i)
+    print(goal_points)
+    while (np.size(at_pose(np.vstack((x_si,x[2,:])), goal_points, position_error=.15, rotation_error=100)) != N):
+
+        # Get poses of agents
+        x = r.get_poses()
+        x_si = uni_to_si_states(x)
+
+        # Create single-integrator control inputs
+        dxi = single_integrator_position_controller(x_si, goal_points[:2][:])
+
+        # Create safe control inputs (i.e., no collisions)
+        dxi = si_barrier_cert(dxi, x_si)
+
+        # Transform single integrator velocity commands to unicycle
+        dxu = si_to_uni_dyn(dxi, x)
+
+        # Set the velocities by mapping the single-integrator inputs to unciycle inputs
+        r.set_velocities(np.arange(N), dxu)
+        # Iterate the simulation
+        r.step()
+
+#Call at end of script to print debug information and for your script to run on the Robotarium server properly
+r.call_at_scripts_end()
